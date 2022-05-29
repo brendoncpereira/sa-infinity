54b92c43d78422be10648c7f5843ee6ddb1b38b4
brendoncpereira <brendoncastrop@gmail.com>
2022-05-29 00:28:52 -0300

    test responsividade

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,7 +8,7 @@ const Home = () => {
             <header>
                 <img src={logo}/>
                 
-                <h2>Escolha como você gostaria de se cadastrar.</h2>
+                <h3>Escolha como você gostaria de se cadastrar.</h3>
                 
             </header>
 
@@ -20,11 +20,9 @@ const Home = () => {
                         
                         <h1 className='title'>Aluno</h1>
 
-                        <div className='first-wrap'>
-                        <div className='sec-wrap'>
-                            <h3 className='text'>Encontre professores que te auxiliem nos estudos nas disciplinas de sua escolha.</h3>
-                        </div>
-                    </div>
+                      
+                            <p className='text'>Encontre professores que te auxiliem nos estudos nas disciplinas de sua escolha.</p>
+                        
 
                         <div className='btn-wrapper'> 
                             <button>Entrar</button>
@@ -40,11 +38,9 @@ const Home = () => {
                         
                         <h1 className='title'>Professor</h1>
 
-                        <div className='first-wrap'>
-                        <div className='sec-wrap'>
-                            <h3 className='text'>Contribua de forma voluntária no conhecimento de estudantes de todo o país.</h3>
-                        </div>
-                    </div>
+                     
+                            <p className='text'>Contribua de forma voluntária no conhecimento de estudantes de todo o país.</p>
+                      
                         
                         
                         <div className='btn-wrapper'> 
